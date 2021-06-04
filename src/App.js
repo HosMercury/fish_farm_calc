d80e320a3b77b18b90ec297d555d3ab4ba2947d9
HosMercury <hos.mercury@gmail.com>
2021-06-04 23:46:23 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,254 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import App2 from './App2';
+import 'bulma/css/bulma.min.css';
+const App = () => {
+  const [state, setState] = useState({
+    totalFishMass: 25000,
+    totalVolumeOfWater: 500,
+    proposedDailyFeed: 1,
+    feedingRatio: 4,
+    feedProtein: 28,
+    nitrogen: 0.16,
+    wasteNitrogen: 0.61,
+    NH3: 1.2,
+    initialWeight: 1000000,
+    finalWeight: 1500000,
+    numberOfDays: 14,
+    waterTemperature: 25,
+    ph: 8,
+    f: 0.05,
+    tanConcentration: 1.336,
+    proposedMass: null,
+    averageWeightGain: null,
+    averageDailyGain: null,
+    specificGrowthRate: null,
+    FCR: null,
+    feedEffiency: null,
+    toxicAmonia: null,
+    tan: null
+  });
 
-function App() {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    console.log(e.target.value);
+    setState({
+      ...state,
+      [e.target.name]: parseFloat(value)
+    });
+  };
+
+  const quantityFeedPerDay = () => {
+    return (state.feedingRatio * state.initialWeight) / 100000;
+  };
+
+  const fcr = () => {
+    return (
+      (quantityFeedPerDay() * state.numberOfDays) /
+      (state.finalWeight - state.initialWeight)
+    );
+  };
+
+  console.log(state.tanConcentration);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box has-text-centered">
+        <h1 className="title is-2">Growth Performance</h1> <hr />
+        <div className="columns">
+          <div className="column">
+            <label className="label">Total fish mass (kg)</label>
+            <input
+              name="totalFishMass"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.totalFishMass.toString()}
+              className="input is-warning"
+            />
+            <h6 className="title is-6 ">{state.totalFishMass / 1000} ton</h6>
+          </div>
+          <div className="column">
+            <label className="label">Total volume of water (m^3)</label>
+            <input
+              name="totalVolumeOfWater"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.totalVolumeOfWater}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">Feeding ratio (%)</label>
+            <input
+              name="feedingRatio"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.feedingRatio}
+              className="input is-warning"
+            />
+            <h6 className="title is-6 ">daily feed</h6>
+          </div>
+          <div className="column">
+            <label className="label">Feed Protein</label>
+            <input
+              name="feedProtein"
+              onChange={(e) => handleChange(e)}
+              className="input is-warning"
+              defaultValue={state.feedProtein.toString()}
+            />
+            <h6 className="title is-6 ">100 g feed</h6>
+          </div>
+          <div className="column">
+            <label className="label">Initial weight</label>
+            <input
+              name="initialWeight"
+              onChange={(e) => handleChange(e)}
+              className="input is-warning"
+              defaultValue={state.initialWeight.toString()}
+            />
+            <h6 className="title is-6 ">{state.initialWeight / 1000} kg</h6>
+          </div>
+          <div className="column">
+            <label className="label">Final weight</label>
+            <input
+              name="finalWeight"
+              onChange={(e) => handleChange(e)}
+              className="input is-warning"
+              defaultValue={state.finalWeight.toString()}
+            />
+            <h6 className="title is-6 ">{state.finalWeight / 1000} kg</h6>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <label className="label">Number of days (rearing time)</label>
+            <input
+              name="numberOfDays"
+              onChange={(e) => handleChange(e)}
+              className="input is-warning"
+              defaultValue={state.numberOfDays.toString()}
+            />
+            <h6 className="title is-6 ">day</h6>
+          </div>{' '}
+          <div className="column">
+            <label className="label">Water temperature</label>
+            <input
+              name="waterTemperature"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.waterTemperature.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">PH</label>
+            <input
+              name="ph"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.ph.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">F</label>
+            <input
+              name="f"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.f.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">TAN concentration (mg/l)</label>
+            <input
+              name="tanConcentration"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.tanConcentration.toString()}
+              className="input is-warning"
+            />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column down">
+            <label className="label">Proposed stocking density (kg/m^3)</label>
+            <div className="title is-5">
+              {state.totalFishMass / state.totalVolumeOfWater}
+            </div>
+          </div>
+          <div className="column down">
+            <label className="label">Quantity feed per day</label>
+            <h6 className="title is-5">
+              {quantityFeedPerDay()}kg ={quantityFeedPerDay() * 1000} g
+            </h6>
+          </div>
+          <div className="column down">
+            <label className="label">Quantity of protein (kg)</label>
+            <div className="title is-5">
+              {((state.feedProtein / 100) * quantityFeedPerDay()).toPrecision(
+                5
+              )}{' '}
+              kg =
+              {(
+                (state.feedProtein / 100) *
+                quantityFeedPerDay() *
+                1000
+              ).toPrecision(5)}{' '}
+              g
+            </div>
+          </div>
+          <div className="column down">
+            <label className="label">Weight gain</label>
+            <div className="title is-5">
+              {(state.finalWeight - state.initialWeight) / 1000} kg =
+              {state.finalWeight - state.initialWeight} g
+            </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column down">
+            <label className="label">Average daily gain (g/day)</label>
+            <div className="title is-5">
+              {(
+                (state.finalWeight - state.initialWeight) /
+                state.numberOfDays /
+                1000
+              ).toPrecision(5)}
+              g
+            </div>
+          </div>
+          <div className="column down">
+            <label className="label">Specific growth rate</label>
+            <div className="title is-5">
+              {(
+                ((Math.log(state.finalWeight) / Math.log(10) -
+                  Math.log(state.initialWeight) / Math.log(10)) /
+                  14) *
+                100
+              ).toPrecision(5)}
+            </div>
+          </div>
+          <div className="column down">
+            <label className="label">FCR</label>
+            <div className="title is-5">{fcr()}</div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Feed effiency</label>
+            <div className="title is-5">
+              {((1 / fcr()) * 100).toPrecision(5)}
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Toxic ammonia (mg/l)</label>
+            <div className="title is-5">
+              {(state.tanConcentration * state.f).toPrecision(3)}
+            </div>
+          </div>
+
+          {/* <div className="column down">
+            <label className="label">Tan</label>
+            <div className="title is-5"></div>
+          </div> */}
+        </div>
+      </div>
+      <App2 />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,270 @@
+import React, { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+const App = () => {
+  const [state, setState] = useState({
+    fishMass: 25000,
+    proposedDailyFeed: 1,
+    feedingRatio: 2,
+    feedProtein: 28,
+    totalWaterVolume: 500,
+    estimatedFilterExchangeRate: 1,
+    mediaSSA: 500,
+    amountOfAirPerM3: 1550,
+    protein: 16,
+    nitrogen: 16,
+    wasteNitrogen: 61,
+    NH3: 1.2,
+    ammoniaConversationRate: 0.57,
+    quantityFeedPerDay: null
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.name);
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: parseInt(value)
+    });
+  };
+
+  // console.log(state);
+
+  return (
+    <>
+      <div className="box has-text-centered">
+        <h1 className="title is-2">RAS Design</h1> <hr />
+        <div className="columns">
+          <div className="column">
+            <label className="label">Fish Mass (kg)</label>
+            <input
+              name="fishMass"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.fishMass.toString()}
+              className="input is-warning"
+            />
+            <h6 className="title is-6 ">{state.fishMass / 1000} ton</h6>
+          </div>
+
+          <div className="column">
+            <label className="label">Proposed daily feed (kg)</label>
+            <input
+              name="proposedDailyFeed"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.proposedDailyFeed.toString()}
+              className="input is-warning"
+            />
+          </div>
+
+          <div className="column">
+            <label className="label">Feeding ratio (%)</label>
+            <input
+              name="feedingRatio"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.feedingRatio.toString()}
+              className="input is-warning"
+            />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <label className="label">Total water volume (m^3)</label>
+            <input
+              name="totalWaterVolume"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.totalWaterVolume.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">
+              Estimated filter exchange rate time/hour
+            </label>
+            <input
+              name="estimatedFilterExchangeRate"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.estimatedFilterExchangeRate.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">Media SSA (M2)</label>
+            <input
+              name="mediaSSA"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.mediaSSA.toString()}
+              className="input is-warning"
+            />
+          </div>
+          <div className="column">
+            <label className="label">
+              Amount of air per m3 produced by air blower
+            </label>
+            <input
+              name="amountOfAirPerM3"
+              onChange={(e) => handleChange(e)}
+              defaultValue={state.amountOfAirPerM3.toString()}
+              className="input is-warning"
+            />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column  down">
+            <label className="label">Quantity feed per day</label>
+            <div className="title is-5">
+              {(state.fishMass * state.feedingRatio) / 100} kg =
+              {((state.fishMass * state.feedingRatio) / 100) * 1000} g
+            </div>
+          </div>
+
+          {/* <div className="column">
+            <label className="label">Nitrogen (g)</label>
+            <div className="title is-5">{state.nitrogen}</div>
+          </div>
+          <div className="column">
+            <label className="label">Waste nitrogen (g)</label>
+            <div className="title is-5">{state.wasteNitrogen}</div>
+          </div>
+          <div className="column">
+            <label className="label">NH3 (g)</label>
+            <div className="title is-5">{state.NH3}</div>
+          </div> */}
+          {/* <div className="column">
+            <label className="label">Ammonia conversion rate/m2/day (g)</label>
+            <div className="title is-5">{state.ammoniaConversationRate}</div>
+          </div> */}
+
+          <div className="column down">
+            <label className="label">Stocking density (kg/m^2)</label>
+            <div className="title is-5">
+              {state.fishMass / state.totalWaterVolume}
+            </div>
+          </div>
+
+          <div className="column  down">
+            <label className="label">TAN</label>
+            <div className="title is-5">
+              {state.feedProtein *
+                ((state.fishMass * state.feedingRatio) / 100000000) *
+                state.nitrogen *
+                state.wasteNitrogen *
+                state.NH3}{' '}
+              kg ={' '}
+              {state.feedProtein *
+                ((state.fishMass * state.feedingRatio) / 100000000) *
+                state.nitrogen *
+                state.wasteNitrogen *
+                state.NH3 *
+                1000}
+              g
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">CO2</label>
+            <div className="title is-5">
+              {((state.fishMass * state.feedingRatio * 0.25) / 100 +
+                (state.fishMass * state.feedingRatio * 0.4) / 100) *
+                1.375}
+              kg ={' '}
+              {((state.fishMass * state.feedingRatio * 0.25) / 100 +
+                (state.fishMass * state.feedingRatio * 0.4) / 100) *
+                1.375 *
+                1000}{' '}
+              gm
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Oxogen required for fish/day</label>
+            <div className="title is-5">
+              {(state.fishMass * state.feedingRatio * 0.25) / 100} kg =
+              {((state.fishMass * state.feedingRatio * 0.25) / 100) * 1000} g
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Oxogen required for bacteria</label>
+            <div className="title is-5">
+              {(state.fishMass * state.feedingRatio * 0.4) / 100} kg =
+              {((state.fishMass * state.feedingRatio * 0.4) / 100) * 1000} g
+            </div>
+          </div>
+        </div>
+        <div className="columns ">
+          <div className="column down">
+            <label className="label">Solid produced per day</label>
+            <div className="title is-5">
+              {(state.fishMass * state.feedingRatio * 0.25) / 100} kg =
+              {((state.fishMass * state.feedingRatio * 0.25) / 100) * 1000} g
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">
+              Maximum concentration of totla ammonia (TAN mg/l)
+            </label>
+            <div className="title is-5">
+              {((state.feedProtein *
+                ((state.fishMass * state.feedingRatio) / 100000000) *
+                state.nitrogen *
+                state.wasteNitrogen *
+                state.NH3 *
+                1000) /
+                (state.totalWaterVolume * 24000)) *
+                1000}
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Filter operating surface area (m^2)</label>
+            <div className="title is-5">
+              {(
+                (state.feedProtein *
+                  ((state.fishMass * state.feedingRatio) / 100000) *
+                  state.nitrogen *
+                  state.wasteNitrogen *
+                  state.NH3) /
+                0.57 /
+                1000
+              ).toPrecision(6)}
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">Total media / day (m3)</label>
+            <div className="title is-5">
+              {(
+                (state.feedProtein *
+                  ((state.fishMass * state.feedingRatio) / 100000) *
+                  state.nitrogen *
+                  state.wasteNitrogen *
+                  state.NH3) /
+                0.57 /
+                state.mediaSSA
+              ).toPrecision(6)}
+            </div>
+          </div>
+
+          <div className="column down">
+            <label className="label">
+              Amount of oxygen produced by blower (g)
+            </label>
+            <div className="title is-5">{state.amountOfAirPerM3 * 210}</div>
+          </div>
+
+          <div className="column down">
+            <label className="label">
+              Amount of DO in water from blower (g)
+            </label>
+            <div className="title is-5">
+              {state.amountOfAirPerM3 * 210 * 0.03}
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr /> <br />
+    </>
+  );
+};
+
+export default App;
